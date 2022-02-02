f1e8f3bfffa0df498baafd55a21d12a387f13e86
wdingsoft <wdingbox@gmail.com>
2022-02-01 19:57:38 -0500

    update

---
+++ idxobj.json.js
@@ -0,0 +1,21 @@
+var idxobj = {
+    "./": [
+        {
+            "idxor.html": 1455
+        }
+    ],
+    "bjtrip": [],
+    "bjtrip/info": [
+        {
+            "familymembers.htm": 24536
+        }
+    ],
+    "bjtrip/info/js": [
+        {
+            "jq.js": 89475
+        },
+        {
+            "jq_tablesorter.js": 142134
+        }
+    ]
+}
